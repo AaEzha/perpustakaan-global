97d244ee6b60257f47825dd6f6fd96c7114b3bf2
AaEzha <rznrfchm@gmail.com>
2024-05-11 11:40:09 +0700

    chore: improve security 🔏

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Model::preventLazyLoading();
+        Model::preventAccessingMissingAttributes();
+
         Gate::define('admin', function (User $user) {
             return $user->title === User::TITLE_ADMIN;
         });
